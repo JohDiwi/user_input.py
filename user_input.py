4a1636c9463f92809f22ee6b77ff7fcf56f75ee2
JohDiwi <162934025+JohDiwi@users.noreply.github.com>
2024-03-11 04:00:48 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+My_name_is=input("Your name is:")
+name=(My_name_is)
+My_age_is=input("How old are you?:")
+age=(My_age_is)
+Where_is_your_location=input("Where do you stay?:")
+location=(Where_is_your_location)
+print("My name is " +name + ", I am " +age+ "i stay "+location)
+
+
